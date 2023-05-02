2884e5855318b8aed49841aa6b6f11aed60658b5
Emilly Mello <emill.ymello212@gmail.com>
2023-05-02 20:57:15 -0300

    1005

---
+++ javascript/problems/1005.js
@@ -0,0 +1,8 @@
+export function problem (lines){
+    const a = Number(lines[0]);
+    const b = Number(lines[1]);
+    const media = (a * 3.5 + b * 7.5)/11;
+
+    console.log('MEDIA = ' + media);
+    
+}
